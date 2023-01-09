eaf7533a5655581b1851deeb094bf34bbe05baf4
lester wee <lester.wee.68@gmail.com>
2023-01-10 02:48:39 +0800

    removed initial index.css, replaced with custom.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './custom.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
